b7c1fc9b1a99d33d7f71799cbc638731a0795c20
saitejareddy007 <tejareddy.d321@gmail.com>
2017-06-28 12:39:19 +0530

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,23 @@
 /**
  * Created by Sai on 6/27/17.
  */
+$(document).ready(function(){
+    var readMoreHtml = $(".comments_page").html();
+    var invisible = readMoreHtml.substr(0,0);
+
+    $(".comments_page").html(invisible).append("<a href='' class='view' > view comments</a>");
+
+    $("body").on("click",".view",function (event) {
+        event.preventDefault();
+        $(this).parent(".comments_page").html(readMoreHtml).append("<a href='' class='hide_view' > hide comments</a>");
+    });
+
+    $("body").on("click",".hide_view",function (event) {
+        event.preventDefault();
+        $(this).parent(".comments_page").html(invisible).append("<a href='' class='view' > hide comments</a>");
+    });
+})
+
 function validateForm() {
     var a=document.forms["Form"]["Name"].value;
     var b=document.forms["Form"]["post"].value;
@@ -20,7 +37,7 @@ function formvalidate(){
     var a=document.forms["postLayout"]["comment"].value;
 
     if(a==null||a==""){
-        alert("blank comment is not valid")
+        alert("Blank comment is not valid")
         return false;
     }
 }
