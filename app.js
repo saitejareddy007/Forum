bd81905f8905af858170202aee3984f8f3207c1b
saitejareddy007 <tejareddy.d321@gmail.com>
2017-06-30 00:24:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var index = require('./routes/index');
 
 // view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
