5b028e1e4c7689d95866b5321ce6e446db1e257e
saitejareddy007 <tejareddy.d321@gmail.com>
2017-06-29 20:50:59 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,10 @@
+/**
+ * Created by Sai on 6/29/17.
+ */
 var express = require('express');
-var router = express.Router();
-var url = "mongodb://localhost:27017/MongoDatabase";
-var mongoose = require('mongoose');
-var Schema = require('Schema');
-var MongoClient =require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
+var router = express();
+var index = require('../lib/home');
 
+router.use('/',index);
 
-var dbSchema =new mongoose.Schema({
-    Name    :   String,
-    Post    :   String,
-    date    :   String,
-    comments:   [
-        {
-            content :   String,
-            date    :   String
-        }
-    ]
-});
-mongoose.connect(url);
-var user = mongoose.model("employees",dbSchema);
-
-/* GET home page. */
-
-router.get('/',function(req,res){
-    user.find({},function (err, docs) {
-        if(err) res.json(err);
-        else res.render('index',{users:docs});
-    });
-});
-
-router.post('/new',function (req, res) {
-
-    var date = new Date();
-    new user({
-        Name    :   req.body.Name,
-        Post    :   req.body.post,
-        date    :   date.toString().substr(0,24)
-    }).save();
-    res.redirect('/');
-
-});
-
-router.post('/:date',function (req, res) {
-    var date = req.params.date;
-   // console.log(name);
-    var comdate = new Date();
-    if(!req.body.comment==""){
-        //res.send('<script>alert("Blank comment is not valid.")</script>')
-        MongoClient.connect(url, function (err, db) {
-            db.collection("employees").update({date: date},
-                {
-                    $push: {
-                        comments: {
-                            "content": req.body.comment,
-                            "createdAt": comdate
-                        }
-                    }
-                });
-        });
-    }
-    res.redirect("/");
-});
-
-module.exports = router;
+module.exports=router;
