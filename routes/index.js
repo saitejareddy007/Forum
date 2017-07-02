9e99a43cae0eb6901b03db06314b84be239b206d
saitejareddy007 <tejareddy.d321@gmail.com>
2017-07-02 21:19:28 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,25 @@
-/**
- * Created by Sai on 6/29/17.
- */
 var express = require('express');
-var router = express.Router();
-var db = require('../repo/db');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
 
-/* GET home page. */
-router.get('/',function(req,res){
-    db.find({},function (err, docs) {
-        if(err) res.json(err);
-        else res.render('index',{users:docs});
-    });
-});
+var index = require('./routes/index');
 
-router.post('/new',function (req, res) {
-    var date = new Date();
-    new db({
-        Name    :   req.body.Name,
-        Post    :   req.body.post,
-        date    :   date.toString().substr(0,24)
-    }).save();
-    res.redirect('/');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-router.post('/:date',function (req, res) {
-    var date    =   req.params.date;
-    var comdate =   new Date().toString().substr(0,24);
-    var query   =   {date: date};
-    var reqb    =   req.body;
-    if(!reqb.comment==""){
-        //res.send('<script>alert("Blank comment is not valid.")</script>')
-        db.update(query,
-            {
-                $push: {
-                    comments: {
-                        "content": reqb.comment ,
-                        "createdAt": comdate
-                    }
-                }
-            },function (err) {
-                if (err) throw err;
-            }
-        );
-    }
-    res.redirect("/");
-});
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-module.exports=router;
+app.use('/',index);
+
+module.exports = app;
