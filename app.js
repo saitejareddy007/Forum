e4b25285d95089dd54f47b7732f3c8728f7fc444
saitejareddy007 <tejareddy.d321@gmail.com>
2017-07-02 21:18:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,19 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+app.use('/',index);
 
 module.exports = app;
