8d5d1aa2cdb2277227a03b745f9f9836a80773f3
saitejareddy007 <tejareddy.d321@gmail.com>
2017-07-02 21:20:15 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
+/**
+ * Created by Sai on 6/29/17.
+ */
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
+var router = express.Router();
+var db = require('../repo/db');
+var homeApi = require('../lib/home');
+require('es6-shim');
 
-var index = require('./routes/index');
+/* GET home page. */
+router.get('/',function(req,response){
+    homeApi.getAllElements(response);
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+router.post('/new',function (req, res) {
+    homeApi.createPost(req.body.Name,req.body.post);
+    res.redirect('/');
+});
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+router.post('/:date',function (req, res) {
+    homeApi.addComment(req.params.date,req.body.comment);
+    res.redirect("/");
+});
 
-app.use('/',index);
-
-module.exports = app;
+module.exports=router;
