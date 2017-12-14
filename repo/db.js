60d4630f013c34825d76f44b734462a9d405264e
saitejareddy007 <tejareddy.d321@gmail.com>
2017-12-14 20:37:45 +0530

    Database modification for login

--- repo/db.js
+++ repo/db.js
@@ -1,20 +1,40 @@
 /**
  * Created by Sai on 6/29/17.
  */
-var express = require('express');
+
 var url = "mongodb://localhost:27017/MongoDatabase";
 var mongoose = require('mongoose');
-var dbSchema =new mongoose.Schema({
-    Name    :   String,
-    Post    :   String,
-    date    :   String,
-    comments:   [
-        {   content     :   String,
-            createdAt   :   String
-        }
-    ]
+
+var accountSchema   =   new mongoose.Schema({
+    Name        :   String,
+    Email       :   String,
+    Password    :   String
 });
-var db = mongoose.model("employees",dbSchema);
+var ItemSchema = new mongoose.Schema(
+    { img:
+        { data: Buffer, contentType: String }
+    }
+);
+var item = mongoose.model("Avathar",ItemSchema);
+
+var postSchema      =   new mongoose.Schema({
+    idOfPost    :   String,
+    Name        :   String,
+    Post        :   String,
+    date        :   String,
+    comments    :   [
+                        {
+                            content: String,
+                            createdAt: String
+                        }
+                    ]
+});
+
+var dbOfPosts       =   mongoose.model("Posts",postSchema);
+
+var dbOfAccounts    =   mongoose.model("Users",accountSchema);
+
+var db={dbOfPosts,dbOfAccounts,item};
 
 mongoose.connect(url);
 
