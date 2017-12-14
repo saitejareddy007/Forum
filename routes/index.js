02b63a7eee3af7d01f01314dddd70e8ca936dac3
saitejareddy007 <tejareddy.d321@gmail.com>
2017-12-14 20:39:32 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,80 +1,111 @@
+/**
+ * Created by Sai on 6/29/17.
+ */
+
+var express = require('express');
+var session = require('express-session');
 var db = require('../repo/db');
+var fs =require('fs');
+var path= require('path');
+var router = express.Router();
+var homeApi = require('../lib/home').getHomeAPIInstance();
+var isset = require('isset');
+var sess;
 
-class Home {
-    getAllElements(params,cb1,cb2) {
-        const postParams = params.postParams;
-        const email = postParams.loginId;
-        const password = postParams.password;
-        db.dbOfAccounts.findOne({Email:email.toString()},function (err,result) {
-            if(!result)
-                cb2();
-
-            else if(password == result.Password){
-                db.dbOfPosts.find({}, cb1)
-            }
-
-            else
-                cb2();
-        });
+/* GET home page. */
+router.get('/',function (req, res, next) {
+    sess = req.session;
+    console.log(sess._id);
+    if (!isset(sess._id)) {
+        res.render('login');
+    } else {
+        next();
     }
-    createForumAccount(params,cb){
-        const postParams = params.postParams;
-        const name = postParams.name;
-        const email = postParams.username;
-        const password = postParams.newPassword;
-        new db.dbOfAccounts({
-            Name:name,
-            Email:email,
-            Password:password
-        }).save(cb);
-    }
+},function(req,res){
+    db.dbOfPosts.find({},function (err, docs) {
+        res.render('index', {posts: docs});
+    })
+});
 
-    createPost(params,cb) {
-        const postParams = params.postParams;
-        const name = params.Name;
-        const id=   params._id;
-        console.log(id);
-        const post = postParams.post;
-        var date = new Date().toString().substr(0,24);
-        new db.dbOfPosts(
-            {
-                idOfPost:id,
-                Name:name,
-                Post:post,
-                date:date
-            }
-        ).save(cb);
-    }
-
-    addComment(params,cb) {
-        const id = params.id;
-        const comment = params.postParams && params.postParams.comment || "";
-        var comdate =   new Date().toString().substr(0,24);
-        var query   =   {_id: id};
-        if(!comment==""){
-            db.dbOfPosts.update(query,
-                {
-                    $push: {
-                        comments: {
-                            "content": comment ,
-                            "createdAt": comdate
-                        }
-                    }
-                },cb
-            );
+router.post('/logout',function (req, res) {
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
         }
-    }
+    });
+});
 
-    viewComments(params, cb) {
-        var id =params.id;
-        db.dbOfPosts.findById(id, cb);
-    }
-}
+router.post('/',function(req,res){
+    const params = req.params || {};
+    params.postParams = req.body;
+    sess=req.session;
+    db.dbOfAccounts.findOne({Email:req.body.loginId},function (err, result) {
+        sess._id=result._id;
+        sess.Name=result.Name;
+        console.log("sucessfully passed");
+    });
 
-// returns an application wide static instance of homeAPI
-function getHomeAPIInstance() {
-    const home = new Home();
-    return home;
-}
+    homeApi.getAllElements(params,function(err, docs) {
+        res.render('index', {posts: docs});
+    },function (err) {
+        res.render('login');
+    });
+});
 
-exports.getHomeAPIInstance = getHomeAPIInstance;
+router.post('/newAccount',function (req, res) {
+    const params = req.params || {};
+    params.postParams = req.body;
+
+    homeApi.createForumAccount(params,function(err){
+        if (err) throw err;
+        res.send('Your account has been created successfully<a href="/">click here to login</a>')
+    });
+});
+
+router.post('/new',function (req, res) {
+    // read and construct params var
+
+    var newItem=new db.item();
+    //console.log()
+
+    newItem.img.data=(req.body.file,"base64");
+    newItem.img.contentType = 'image/png';
+    newItem.save();
+
+    const params = req.params;
+    params.postParams = req.body;
+    params._id=sess._id;
+    console.log(sess._id);
+
+    params.Name=sess.Name;
+    homeApi.createPost(params,function (err) {
+        if (err) throw err;
+        res.send({msg:'Your post has been created successfully.'});
+    });
+
+});
+
+router.post('/addcomment/:id',function (req, res) {
+    // read and construct params var
+    const params = req.params || {};
+    params.postParams = req.body;
+
+    homeApi.addComment(params,function (err) {
+        res.redirect('/'+params.id)
+    });
+
+});
+
+router.get('/:id',function (req, res) {
+    const params = req.params || {};
+//    params.postParams = req.body;
+
+    homeApi.viewComments(params,function (err, docs) {
+        res.render('comments', {posts: docs});
+    });
+
+});
+
+module.exports=router;
