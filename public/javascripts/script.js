600c161bf51fe8b6fc14b1eeb9b9f45ed9fb4b21
saitejareddy007 <tejareddy.d321@gmail.com>
2018-06-10 11:47:29 +0530

    updated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,6 +27,9 @@ function validateForm() {
 
 function postAnimation() {
     $(".main_content").animate({'margin-top': '0px','opacity':'1'},900);
+    $("h1").click(function () {
+        alert(window.outerWidth);
+    });
 }
 
 function loadnew() {
