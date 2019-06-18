686415f85fbf484fc9d4fda4cf9e664171fa4d59
saitejareddy007 <tejareddy.d321@gmail.com>
2019-06-18 11:33:50 +0530

    app.js updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors')
 
 var index = require('./routes/index');
 
@@ -19,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors())
 app.use('/',index);
 
 module.exports = app;
