6f05ce3b63f65814e87d3978fc17bfb09a235e38
saitejareddy007 <tejareddy.d321@gmail.com>
2019-06-18 11:35:59 +0530

    router functions added and router updated with callAPI

--- repo/db.js
+++ repo/db.js
@@ -1,20 +1,21 @@
 /**
  * Created by Sai on 6/29/17.
  */
-var url = "mongodb://localhost:27017/MongoDatabase";
-var mongoose = require('mongoose');
-var dbSchema =new mongoose.Schema({
-    Name    :   String,
-    Post    :   String,
-    date    :   String,
-    comments:   [
-        {   content     :   String,
-            createdAt   :   String
+const url = "mongodb://localhost:27017/MongoDatabase";
+let mongoose = require('mongoose');
+let dbSchema = new mongoose.Schema({
+    Name: String,
+    Post: String,
+    date: String,
+    comments: [
+        {
+            content: String,
+            createdAt: String
         }
     ]
 });
-var db = mongoose.model("employees",dbSchema);
 
-mongoose.connect(url);
+let db = mongoose.model("employees", dbSchema);
+const connection = mongoose.connect(url,{ useNewUrlParser: true });
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,45 @@
 /**
  * Created by Sai on 6/29/17.
  */
-var express = require('express');
-var router = express.Router();
-var db = require('../repo/db');
-var homeApi = require('../lib/home').getHomeAPIInstance();
+let express = require('express');
+let router = express.Router();
+let db = require('../repo/db');
+let homeApi = require('../lib/homeAPI').getInstance();
+let {callAPI} = require('../lib/common-utils/router_functions');
+let fn = require('../lib/common-utils/functions');
+let {renderViewFromAPI} = require('../lib/common-utils/router_functions');
 
-//require('es6-shim');
+// require('es6-shim');
 
 /* GET home page. */
-router.get('/',function(req,res){
-    homeApi.getAllElements(function(err, docs) {
-        res.render('index', {users: docs});
-    });
+router.get('/', (req, res) => {
+    renderViewFromAPI(req, res, fn.bind(homeApi,"getAllElements"), 'index');
+});
+
+router.post("/getPosts/", (req, res) => {
+    callAPI(req, res, fn.bind(homeApi,"getAllElements"));
 });
 
 router.post('/new',function (req, res) {
-    // read and construct params var
-    const params = req.params || {};
-    params.postParams = req.body;
-
-    homeApi.createPost(params,function (err) {
-        if (err) throw err;
-        res.send({msg:"Your post has been created successfully."});
-    });
-
+    callAPI(req, res, fn.bind(homeApi,"createPost"));
 });
 
-router.post('/addcomment/:id',function (req, res) {
+router.post('/add-comment/:id',function (req, res) {
     // read and construct params var
     const params = req.params || {};
     params.postParams = req.body;
 
-    homeApi.addComment(params,function (err) {
+    Home.addComment(params,function (err) {
         res.redirect("/"+req.params.id);
     });
 
 });
 router.post('/deletePost/:id',function (req, res) {
-    const params = req.params || {};
-    homeApi.deletePost(params,function (err, result) {
-        if(err) throw err;
-        res.end("The post is deleted successfully");
-    });
-})
-router.get('/:id',function (req, res) {
-    const params = req.params || {};
-//    params.postParams = req.body;
+    callAPI(req, res, fn.bind(homeApi,"deletePost"));
+});
 
-    homeApi.viewComments(params,function (err, docs) {
-        res.render('comments', {user: docs});
-    });
-
+router.get('/view-comments/:id',function (req, res) {
+    renderViewFromAPI(req, res, fn.bind(homeApi,"viewComments"),"comments");
 });
 
 module.exports=router;
