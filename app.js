5e0850c3180a4c3e7e22c8b854b0d02412998933
saitejareddy007 <tejareddy.d321@gmail.com>
2019-07-26 10:56:25 +0530

    updated to latest

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
-var cors = require('cors')
-
-var index = require('./routes/index');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const index = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
